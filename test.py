ff80101efc1ad6c389f289b6ced7ef7916fc58b0
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2022-01-14 19:18:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ from GeneratePattern import GeneratePattern
 obj = GeneratePattern([1,2,3,4,5,6,7,8,9,10], 9)
 input_, output = obj.generate()
 
-print(input_)
+print("Input Sequences")
+print(*input_, sep="\n")
+print("\nCorresponding Outputs")
 print(output)
